cf4afee208bab8fde7e7b2b57445693537924c69
Anna Hutsuliak <anhutsuliak@gmail.com>
2019-12-25 00:00:32 +0200

    first commit

---
+++ src/com/epam/hometask1add/hometask1add.java
@@ -0,0 +1,15 @@
+package com.epam.hometask1add;
+import java.util.Scanner;
+
+public class hometask1add {
+    public static void main (String [] arg) {
+
+        Scanner sc= new Scanner( System.in);
+        String line = sc.nextLine();
+        System.out.println("*  *   ****  *      *       **");
+        System.out.println("*  *   *     *      *      *  *");
+        System.out.println("****   ***   *      *     *    *");
+        System.out.println("*  *   *     *  *   *  *   *  *");
+        System.out.println("*  *   ****  ****   ****    **");
+    }
+}
