b78c110c861ad41ce1b3bf110e48fe349bcafb06
AnnaHutsuliak <59147033+AnnaHutsuliak@users.noreply.github.com>
2020-03-10 15:30:23 +0200

    added phrase "Print word 'HELLO'"

--- src/com/epam/hometask1add/hometask1add.java
+++ src/com/epam/hometask1add/hometask1add.java
@@ -2,8 +2,7 @@ package com.epam.hometask1add;
 import java.util.Scanner;
 
 public class Hometask1add {
-    public static void main (String [] arg) {
-
+      System.out.println("Print word 'HELLO'\n-------------------------------------------");
         Scanner sc= new Scanner( System.in);
         String line = sc.nextLine();
         System.out.println("*  *   ****  *      *       **");
